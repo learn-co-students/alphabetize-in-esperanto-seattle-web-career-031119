0fded464db00c8665b1fd00edf92d3e942816466
Doug Ward <ward92@gmail.com>
2019-02-27 16:48:17 -0800

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  sorted = arr.sort_by do |phrase|
+    phrase.split('').map do |letter|    #split phrase into array of letters, then call map on them
+      ESPERANTO_ALPHABET.index(letter)  #the map returns an array of numbers. The numbers are the index of the letter's position in the alphabet. Sort_by then sorts based on this numerical representation
+    end
+  end
 end
