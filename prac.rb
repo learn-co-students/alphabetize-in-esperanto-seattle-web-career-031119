f89153bef69f007cd3dd10fb49ae7a587168495e
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-03-01 07:57:20 +0000

    Done.

---
+++ prac.rb
@@ -0,0 +1,18 @@
+# arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi"]
+# greetings = ["ŝĝĉĥŭĵ", "ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ"]
+
+# # arr = ["ad", "aa", " ", "ab", "aac", "ĝ", "c", "ĉ", "ad", "fŭ", "fu"]
+
+
+
+
+# hash = {"hello" => ""}
+
+# puts hash["hello"].empty?
+
+
+string = "ĉu ĉvi"
+
+string2 = string.sub(/[ĉĝĥŝŭ]/, '*')
+string2 = string2.sub(/[ĉĝĥŝŭ]/, '*')
+puts string2
