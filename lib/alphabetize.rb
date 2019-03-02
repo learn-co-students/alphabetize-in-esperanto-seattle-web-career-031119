2aa1813e5903c0e1493de9c1c052a28ebf87ebae
jyang81 <joe.yang.design@gmail.com>
2019-03-02 13:42:57 -0800

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+    arr.sort_by do |phrase|
+        phrase.chars.map do |char|
+        ESPERANTO_ALPHABET.index(char)
+        end
+#        binding.pry
+    end
 end
+
